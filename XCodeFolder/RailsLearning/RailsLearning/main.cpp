81d7e896cd7eea1de52901d70a9f8c7404aeca8a
Nandeep Nagarkar <n4n9445@gmail.com>
2013-01-14 09:07:11 -0800

    Jan 14 2013
    learning resumed

---
+++ XCodeFolder/RailsLearning/RailsLearning/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RailsLearning
+//
+//  Created by Nandeep Nagarkar on 1/12/13.
+//  Copyright (c) 2013 CLoudSam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
